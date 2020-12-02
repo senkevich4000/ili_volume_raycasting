aa9133660834b80bb18c83b473ec14731b98fcaf
Konstantin Senkevich <konstantin.senkevich@atomicus.de>
2020-12-02 21:41:58 +0000

    fix typo in test.

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ all:
 
 install:
 	npm install
+
+test:
+	npm test
--- public/js/VolumeUtils.test.js
+++ public/js/VolumeUtils.test.js
@@ -1,9 +1,8 @@
-//import {createIntensityVolume} from './VolumeUtils.js';
-const m = require('./VolumeUtils');
+const volumeUtils = require('./VolumeUtils');
 
 test('Intensity volume test', () => {
   const size = 3;
-  const intensityVolume = m.createIntensityVolume(size, size, size);
+  const intensityVolume = volumeUtils.createIntensityVolume(size, size, size);
   console.log(intensityVolume);
   expect(1 + 2).toBe(3);
 });
