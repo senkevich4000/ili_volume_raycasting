04b962550b56bdcb3cc2597dc159dd9fbefd8bf4
Konstantin Senkevich <konstantin.senkevich@atomicus.de>
2020-12-15 15:45:55 +0000

    Optimize data loader flow. Remove redundant copies.

--- public/js/workers/NormalsFactory.js
+++ public/js/workers/NormalsFactory.js
@@ -7,9 +7,16 @@ require(
     ['VolumeUtils', 'DataLoader'],
     function(VolumeUtils, DataLoader) {
       onmessage = function(event) {
+        if (event.data.type === DataLoader.MessageType.waiting) {
+          postMessage(new DataLoader.ResponseMessage(
+              DataLoader.MessageType.initialized, undefined));
+          return;
+        }
         VolumeUtils.calculateNormalsMap(event.data.calculator);
-        postMessage(new DataLoader.ResponseMessage(true, event.data.calculator.volume));
+        postMessage(new DataLoader.ResponseMessage(
+            DataLoader.MessageType.finished, event.data.calculator.volume));
       };
-      postMessage(new DataLoader.ResponseMessage(false, undefined));
+      postMessage(new DataLoader.ResponseMessage(
+          DataLoader.MessageType.initializing, undefined));
     },
 );
